16bf10f425e332e58bc5eb4da2c1ada237d46c34
Boris Molodenkov <boris.molodenkov@gmail.com>
2015-11-21 15:24:40 +0300

    Add custom rules

--- WME_Assist.user.js
+++ WME_Assist.user.js
@@ -48,6 +48,15 @@ function run_wme_assist() {
         this.correct = func;
     }
 
+    var CustomRule = function (oldname, newname) {
+        var title = oldname + ' -> ' + newname;
+        this.oldname = oldname;
+        this.newname = newname;
+        $.extend(this, new Rule(title, function (text) {
+            return text.replace(oldname, newname);
+        }));
+    }
+
     var Rules = function () {
         var rules = [
             new Rule('Incorrect street name', function (text) {
@@ -57,6 +66,19 @@ function run_wme_assist() {
                 return text.replace(/(^| )(ул\.?)( |$)/, '$1улица$3');
             }),
         ];
+        var customRulesIndex = rules.length;
+
+        var onAdd = function (rule) {}
+        var onEdit = function (index, rule) {}
+        var onDelete = function (index) {}
+
+        this.onAdd = function (cb) { onAdd = cb }
+        this.onEdit = function (cb) { onEdit = cb }
+        this.onDelete = function (cb) { onDelete = cb }
+
+        this.get = function (index) {
+            return rules[index + customRulesIndex];
+        }
 
         this.correct = function (text) {
             var newtext = text;
@@ -67,6 +89,49 @@ function run_wme_assist() {
             }
 
             return newtext;
+        }
+
+        var save = function (rules) {
+            if (localStorage) {
+                localStorage.setItem('assistRulesKey', JSON.stringify(rules.slice(customRulesIndex)));
+            }
+        }
+
+        this.load = function () {
+            if (localStorage) {
+                var str = localStorage.getItem('assistRulesKey');
+                if (str) {
+                    var arr = JSON.parse(str);
+                    for (var i = 0; i < arr.length; ++i) {
+                        var rule = arr[i];
+                        this.push(rule.oldname, rule.newname);
+                    }
+                }
+            }
+        }
+
+        this.push = function (oldname, newname) {
+            var rule = new CustomRule(oldname, newname);
+            console.log(rule);
+            rules.push(rule);
+            onAdd(rule);
+
+            save(rules);
+        }
+
+        this.update = function (index, oldname, newname) {
+            var rule = new CustomRule(oldname, newname);
+            rules[index + customRulesIndex] = rule;
+            onEdit(index, rule);
+
+            save(rules);
+        }
+
+        this.remove = function (index) {
+            rules.splice(index + customRulesIndex, 1);
+            onDelete(index);
+
+            save(rules);
         }
     }
 
@@ -150,6 +215,18 @@ function run_wme_assist() {
         section = document.createElement('p');
         section.style.paddingTop = "8px";
         section.style.textIndent = "16px";
+        section.id = "assist_custom_rules";
+        $(section)
+            .append($('<p>').addClass('message').css({'font-weight': 'bold'}).text('Custom rules'))
+            .append($('<button>').prop('id', 'assist_add_custom_rule').addClass('btn btn-default btn-primary').text('Add'))
+            .append($('<button>').prop('id', 'assist_edit_custom_rule').addClass('btn btn-default').text('Edit'))
+            .append($('<button>').prop('id', 'assist_del_custom_rule').addClass('btn btn-default btn-warning').text('Del'))
+            .append($('<ul>').addClass('result-list'));
+        addon.appendChild(section);
+
+        section = document.createElement('p');
+        section.style.paddingTop = "8px";
+        section.style.textIndent = "16px";
         section.id = "assist_minireport";
         section.innerHTML = '<b>Mini report</b><br/>' +
             '<p>Unresolved errors: <span id="assist-error-num">0</span></p>' +
@@ -166,6 +243,48 @@ function run_wme_assist() {
         addon.id = "sidepanel-assist";
         addon.className = "tab-pane";
         $('#user-info > .tab-content').append(addon);
+
+        var selectedCustomRule = -1;
+
+        this.selectedCustomRule = function () {
+            return selectedCustomRule;
+        }
+
+        this.addCustomRule = function (title) {
+            var thisrule = $('<li>').addClass('result').click(function () {
+                selectedCustomRule = $('#assist_custom_rules li.result').index(thisrule);
+                info('index: ' + selectedCustomRule);
+                $('#assist_custom_rules li.result').css({'background-color': ''});
+                $('#assist_custom_rules li.result').removeClass('active');
+                $(this).css({'background-color': 'lightblue'});
+                $(this).addClass('active');
+            }).hover(function () {
+                $(this).css({
+                    cursor: 'pointer',
+                    'background-color': 'lightblue'
+                });
+            }, function () {
+                $(this).css({
+                    cursor: 'auto'
+                });
+                if (!$(this).hasClass('active')) {
+                    $(this).css({
+                        'background-color': ''
+                    });
+                }
+            })
+                .append($('<p>').addClass('additional-info clearfix').text(title))
+                .appendTo($('#assist_custom_rules ul.result-list'));
+        }
+
+        this.updateCustomRule = function (index, title) {
+            $('#assist_custom_rules li.result').eq(index).find('p.additional-info').text(title);
+        }
+
+        this.removeCustomRule = function (index) {
+            $('#assist_custom_rules li.result').eq(index).remove();
+            selectedCustomRule = -1;
+        }
 
         $('<div>').prop('id', 'WME_AssistWindow').css({
             position: 'fixed',
@@ -224,6 +343,48 @@ function run_wme_assist() {
                     })))
             .appendTo($('#map'));
 
+        $('<div>').prop('id', 'assist_custom_rule_dialog')
+            .append($('<p>All form fields are required</p>'))
+            .append($('<form>')
+                    .append($('<fieldset>')
+                            .append($('<label>').prop('for', 'oldname').text('Old name'))
+                            .append($('<input>', {
+                                type: 'text',
+                                name: 'oldname',
+                                'class': 'text ui-widget-content ui-corner-all',
+                                id: 'oldname',
+                            }))
+                            .append($('<label>').prop('for', 'newname').text('New name'))
+                            .append($('<input>', {
+                                type: 'text',
+                                name: 'newname',
+                                'class': 'text ui-widget-content ui-corner-all',
+                                id: 'newname',
+                            }))
+                           )
+                   )
+            .appendTo($('#map'));
+
+        $('#assist_custom_rule_dialog label').css({display: 'block'});
+        $('#assist_custom_rule_dialog input').css({display: 'block', width: '100%'});
+
+        var customRuleDialog_Ok = function () {}
+        var customRuleDialog = $('#assist_custom_rule_dialog').dialog({
+            autoOpen: false,
+            height: 300,
+            width: 350,
+            modal: true,
+            buttons: {
+                Ok: function () {
+                    customRuleDialog_Ok();
+                    customRuleDialog.dialog('close');
+                },
+                Cancel: function () {
+                    customRuleDialog.dialog('close');
+                }
+            }
+        });
+
         this.addProblem = function (id, text, func) {
             $('<li>')
                 .prop('id', 'issue-' + id)
@@ -260,6 +421,10 @@ function run_wme_assist() {
         var showWindowCheckbox = $('#assist_visibility');
         var mainWindow = $('#WME_AssistWindow');
 
+        var addCustomRuleBtn = $('#assist_add_custom_rule');
+        var editCustomRuleBtn = $('#assist_edit_custom_rule');
+        var delCustomRuleBtn = $('#assist_del_custom_rule');
+
         this.fixallBtn = function () { return fixallBtn }
         this.clearfixedBtn = function () { return clearfixedBtn }
         this.resetBtn = function () { return resetBtn }
@@ -271,6 +436,30 @@ function run_wme_assist() {
         this.showWindowCheckbox = function () { return showWindowCheckbox }
 
         this.window = function () { return mainWindow }
+
+        this.addCustomRuleBtn = function () { return addCustomRuleBtn }
+        this.editCustomRuleBtn = function () { return editCustomRuleBtn }
+        this.delCustomRuleBtn = function () { return delCustomRuleBtn }
+        this.customRuleDialog = function (title, params) {
+            var deferred = $.Deferred();
+
+            if (params) {
+                customRuleDialog.find('#oldname').val(params.oldname);
+                customRuleDialog.find('#newname').val(params.newname);
+            }
+
+            customRuleDialog_Ok = function () {
+                deferred.resolve({
+                    oldname: customRuleDialog.find('#oldname').val(),
+                    newname: customRuleDialog.find('#newname').val(),
+                });
+            }
+
+            customRuleDialog.dialog('option', 'title', title);
+            customRuleDialog.dialog('open');
+
+            return deferred.promise();
+        }
 
         mainWindow.hide();
         mainWindow.draggable({handle: 'h1'});
@@ -285,6 +474,20 @@ function run_wme_assist() {
         var action = new ActionHelper(wazeapi);
         var rules = new Rules();
         var ui = new Ui();
+
+        rules.onAdd(function (rule) {
+            ui.addCustomRule(rule.comment);
+        });
+
+        rules.onEdit(function (index, rule) {
+            ui.updateCustomRule(index, rule.comment);
+        });
+
+        rules.onDelete(function (index) {
+            ui.removeCustomRule(index);
+        });
+
+        rules.load();
 
         var problems = [];
         var unresolvedIdx = 0;
@@ -403,6 +606,38 @@ function run_wme_assist() {
                 analyzedIds = [];
                 ui.setUnresolvedErrorNum(0);
                 ui.setFixedErrorNum(0);
+            });
+
+            ui.addCustomRuleBtn().click(function () {
+                ui.customRuleDialog('Add', {
+                    oldname: 'oldname',
+                    newname: 'newname'
+                }).done(function (response) {
+                    rules.push(response.oldname, response.newname);
+                });
+            });
+
+            ui.editCustomRuleBtn().click(function () {
+                var id = ui.selectedCustomRule();
+                if (id >= 0) {
+                    ui.customRuleDialog('Edit', {
+                        oldname: rules.get(id).oldname,
+                        newname: rules.get(id).newname
+                    }).done(function (response) {
+                        rules.update(id, response.oldname, response.newname);
+                    });
+                } else {
+                    alert('Custom rule is not selected');
+                }
+            });
+
+            ui.delCustomRuleBtn().click(function () {
+                var id = ui.selectedCustomRule();
+                if (id >= 0) {
+                    rules.remove(id);
+                } else {
+                    alert('Custom rule is not selected');
+                }
             });
         }
     };
