ab770e9842a931b921383278502d6b32933278b4
Boris Molodenkov <boris.molodenkov@gmail.com>
2016-01-24 15:55:37 +0300

    Bump version 0.2.7

--- WME_Assist.user.js
+++ WME_Assist.user.js
@@ -12,12 +12,12 @@
 // @include   https://editor-beta.waze.com/*
 // @include   https://*.waze.com/editor/editor/*
 // @include   https://*.waze.com/*/editor/*
-// @version   0.2.6
+// @version   0.2.7
 // @namespace https://greasyfork.org/users/20609
 // ==/UserScript==
 
 function run_wme_assist() {
-    var ver = '0.2.6';
+    var ver = '0.2.7';
 
     function debug(message) {
         if (!$('#assist_debug').is(':checked')) return;
