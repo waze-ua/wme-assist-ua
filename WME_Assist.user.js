09201c1b03f56996c26981a09b78bc08cbee0375
Sergey Kuznetsov <sergey@izhevsk.pro>
2016-02-14 20:59:59 +0400

    проверка 36Р-069

--- WME_Assist.user.js
+++ WME_Assist.user.js
@@ -188,6 +188,7 @@ function run_wme_assist() {
                     .replace(/^(\d{2})A-(\d)/, '$1А-$2')
                     .replace(/^(\d{2})K-(\d)/, '$1К-$2')
                     .replace(/^(\d{2})H-(\d)/, '$1Н-$2')
+                    .replace(/^(\d{2})P-(\d)/, '$1Р-$2')
                     .replace(/(\d)А(:|\s+|$)/, '$1A$2')
                     .replace(/(\d)В(:|\s+|$)/, '$1B$2');
                 }),
