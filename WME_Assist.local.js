95fa7859f95bc6f878c940bc70d493a5936940bc
Boris Molodenkov <boris.molodenkov@gmail.com>
2016-02-28 12:18:13 +0300

    Add entry script for local testing

---
+++ WME_Assist.local.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name WME Assist
+// @author borman84 (Boris Molodenkov)
+// @description This script checks and fixes street name for POI and segments
+// @match     https://world.waze.com/editor/*
+// @match     https://*.waze.com/editor/*
+// @match     https://*.waze.com/*/editor/*
+// @match     https://world.waze.com/map-editor/*
+// @match     https://world.waze.com/beta_editor/*
+// @match     https://www.waze.com/map-editor/*
+// @require   file:///home/boris/waze/scaner.js
+// @require   file:///home/boris/waze/analyzer.js
+// @require   file:///home/boris/waze/WME_Assist.user.js
+// @grant     none
+// @include   https://editor-beta.waze.com/*
+// @include   https://*.waze.com/editor/editor/*
+// @include   https://*.waze.com/*/editor/*
+// @version   0.4.7
+// ==/UserScript==
