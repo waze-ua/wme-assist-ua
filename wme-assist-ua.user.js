235b9a52ca5578ca9dc4753eb3cb0d6460a1bd31
Alexander Shashkevich <alex@stunpix.com>
2017-09-18 01:45:26 +0300

    Improved replacing approach
    * Fixed Latin characters replacement (some characters were replaced with 'undefined') and moved at beginning of rules
    * Generic replacements now divided in chain of rules: fix language -> fix errors in short status -> make long status short -> make unnecessary shortenings long.
    * Regexp code has been compacted

--- wme-assist-ua.user.js
+++ wme-assist-ua.user.js
@@ -6,7 +6,7 @@
 // @require      https://github.com/waze-ua/wme-assist-ua/raw/master/analyzer.js
 // @grant        none
 // @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
-// @version      0.5.5
+// @version      0.5.6
 // @namespace    https://greasyfork.org/users/66819
 // @updateURL    https://github.com/waze-ua/wme-assist-ua/raw/master/wme-assist-ua.user.js
 // @downloadURL  https://github.com/waze-ua/wme-assist-ua/raw/master/wme-assist-ua.user.js
@@ -532,190 +532,18 @@ function run_wme_assist() {
         };
 
         var rules_UA = function () {
-            var hasCyrillic = function(s) {return s.search(/[а-яії]/i) != -1;}
+            var hasCyrillic = function(s) {return s.search(/[а-яіїєґ]/i) != -1;}
             var hasShortStatus = function(s) { return s.search(/( |^)(вул\.|просп\.|мкрн\.|наб\.|пл\.|пров\.|ал\.|ст\.|пр\.|ш\.|дор\.|туп\.|б-р|р-н)( |$)/i) != -1; };
             var hasLongStatus = function(s) { return s.search(/( |^)(тракт|узвіз|міст|в\'їзд|виїзд|виїзд|розворот|трамвай|залізниця|майдан|заїзд|траса|шляхопровід|шлях|завулок|квартал)( |$)/i) != -1; };
             var hasSpecialStatus = function(s) { return s.search(/( |^)([РНТМ](-[0-9]+)+|[EОС][0-9]+)( |$)|^(|до|на|>) /i) != -1; };
+            var hasInternationalName = function(s) {return s.search(/^E[0-9]+$/i) != -1; };
             var hasStatus = function(s) { return (hasShortStatus(s) || hasLongStatus(s) || hasSpecialStatus(s)); };
 
-            var isNumber = function (str) {
-                return /([0-9])-[еай]/.test(str);
-            };
-
-            var replaceParts = function (text) {
-                var arr = text.split(' ');
-                var result = [];
-                var status;
-                var number;
-
-                var previousPart = '';
-                for (var i = 0; i < arr.length; ++i) {
-                    var part = arr[i];
-
-                    if (isStatus(part) && !isPseudoStatus(part)) {
-                        status = part;
-                    } else if (isNumber(part) && previousPart.toLowerCase() != 'героїв') {
-                        number = part;
-                    } else {
-                        result.push(part);
-                    }
-
-                    previousPart = part;
-                }
-
-                if (status) {
-                    result.splice(0, 0, status);
-                }
-
-                if (number) {
-                    result.push(number);
-                }
-
-                return result.join(' ');
-            };
-
+            // ATTENTION: Rule order is important!
             return rules_basicCommon().concat([
-                new Rule('Delete space in initials', function (text) {
-                    return text.replace(/(^|\s+)([А-ЯІЇЄ]\.)\s([А-ЯІЇЄ]\.)/, '$1$2$3');
-                }),
-                new Rule('Inapropriate symbol in street name', function (text) {
-                    // This rule should be before other rules or they couldn't see some errors
-                    return text.replace(/[@#№$,^!:;*]"?</, '');
-                }),
-                new Rule('Incorrect apostrophe', function (text) {
-                    return text.replace(/[`’]/g, '\'');
-                }),
-                new Rule('Incorrect language', function (text) {
-                    return text.replace(/(^| )(линия)( |$)/i, '$1лінія$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(тр-т|тракт\.)( |$)/i, '$1тракт$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(провулок|пе?р\.|переулок)( |$)/i, '$1пров.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(туп[іи]к)( |$)/i, '$1туп.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(проспект|пр-т\.?)( |$)/i, '$1просп.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(пр-д\.?|про[їе]зд)( |$)/i, '$1пр.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(улица|в?ул\.?|ул,|вулиц[ая])( |$)/i, '$1вул.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(шосс?е)( |$)/i, '$1ш.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(площа)( |$)/, '$1пл.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(бульвар|буль?в\.|бул\.|бул,?)( |$)/i, '$1б-р$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(станц[иі]я)( |$)/i, '$1ст.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(алл?ея)( |$)/i, '$1ал.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(м[іи]крорайон)( |$)/i, '$1мкрн.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(район)( |$)/i, '$1р-н$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(набережная?)( |$)/i, '$1наб.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return hasShortStatus(text) ? text : text.replace(/(^| )(дорог[аи])( |$)/i, '$1дор.$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return text.replace(/(^| )(сп\.|спуск|узв\.|узвоз)( |$)/i, '$1узвіз$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return text.replace(/(^| )(ген\.?)( |$)/i, '$1Генерала$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return text.replace(/(^| )(марш\.?)( |$)/i, '$1Маршала$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return text.replace(/(^| )(див.\.?)( |$)/i, '$1Дивізії$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return text.replace(/(^| )(ак\.|академика?)( |$)/i, '$1Академіка$3');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return text.replace(/ и /i, ' та ');
-                }),
-                new Rule('Incorrect street name', function (text) {
-                    return text.replace(/-[гштм]а/, '-а').replace(/-[ыоиі]й/, '-й').replace(/-тя/, '-я').replace(/-ая/, '-а');
-                }),
-                new Rule('Incorrect highway name', function (text) {
-                    return text.replace(/([РрНнМмPpHM])[-\s]*([0-9]{2})/, function (a, p1, p2) {
-                        p1 = p1
-                            .replace('р', 'Р')
-                            .replace('н', 'Н')
-                            .replace('м', 'М')
-                            .replace('P', 'Р')
-                            .replace('p', 'Р')
-                            .replace('H', 'Н')
-                            .replace('M', 'М');
-
-                        return p1 + '-' + p2;
-                    });
-                }),
-                new Rule('Incorrect local street name', function (text) {
-                    return text.replace(/([ТтT])[-\s]*([0-9]{2})[-\s]*([0-9]{2})/, function (a, p1, p2, p3) {
-                        p1 = p1
-                            .replace('т', 'Т')
-                            .replace('T', 'Т');
-
-                        return p1 + '-' + p2 + '-' + p3;
-                    });
-                }),
-                new Rule('Incorrect international highway name', function (text) {
-                    return text.replace(/^[\s]*([EeЕе])[-\s]*([0-9]+)/, function (a, p1, p2) {
-                        p1 = p1
-                            .replace('e', 'E')
-                            .replace('е', 'E')
-                            .replace('Е', 'E');
-                        return p1 + p2;
-                    });
-                }),
-                new Rule('Incorrect local road name', function (text) {
-                    return text.replace(/([OoCcОоСс])[-\s]*([0-9]+)[-\s]*([0-9]+)[-\s]*([0-9]+)/, function (a, p1, p2, p3, p4) {
-                        p1 = p1
-                            .replace('o', 'О')
-                            .replace('O', 'О')
-                            .replace('c', 'С')
-                            .replace('C', 'С');
-
-                        return p1 + p2 + p3 + p4;
-                    });
-                }),
-
-                new Rule('Fix status', function (text) {
-                    if (!hasStatus(text)) {
-                        text = 'вул. ' + text;
-                    }
-                    return text;
-                }, 'Ukraine'),
-
-                new Rule('Move status to begin of name', function (text) {
-                    var excludeList = /(?: |^)до |(?: |^)на /;
-                    if (! new RegExp(excludeList).test(text)) {
-                        return text.replace(/(.*)(вул\.)(.*)/, '$2 $1 $3');
-                    }
-                    return text;
-                }, 'Ukraine'),
-
-                new Rule('Fix english caracters in name', function (text) {
+                new Rule('Fix english characters in name', function (t) {
                     // This rule must be applied after all other rules done
-                    return !hasCyrillic(text) ? text : text.replace(/[abcehikmoptxy]/ig, function (c) {
+                    return !hasCyrillic(t) || hasInternationalName(t) ? t : t.replace(/[AaBCcEeHIiKkMOoPpTXxYy]/g, function (c) {
                         return {
                             'A': 'А',
                             'a': 'а',
@@ -742,6 +570,129 @@ function run_wme_assist() {
                         }[c];
                     });
                 }),
+                new Rule('Delete space in initials', function (text) {
+                    return text.replace(/(^| +)([А-ЯІЇЄ]\.) ([А-ЯІЇЄ]\.)/, '$1$2$3');
+                }),
+                new Rule('Disallowed character in street name', function (t) {
+                    // This rule should be before renaming rules or they couldn't see some errors
+                    return t
+                        .replace(/[@#№$,^!:;*"?<]/, '')
+                        .replace(/[`’]/g, '\'');
+                }),
+                new Rule('Incorrect language', function (t) {
+                    // Translate full russian names to full ukrainian
+                    // and next rules will shorten them if necessary
+                    return t
+                        .replace(/(^| )в?улица( |$)/i, '$1вулиця$2')
+                        .replace(/(^| )спуск( |$)/i, '$1узвіз$2')
+                        .replace(/(^| )(т)расса( |$)/i, '$1$2раса$3')
+                        .replace(/(^| )(п)ереулок( |$)/i, '$1$2ровулок$3')
+                        .replace(/(^| )(п)роезд( |$)/i, '$1$2роїзд$3')
+                        .replace(/(^| )(п)лощадь( |$)/i, '$1$2лоща$3')
+                        .replace(/(^| )(ш)оссе( |$)/i, '$1$2осе$3')
+                        .replace(/(^| )(с)танция( |$)/i, '$1$2танція$3')
+                        .replace(/(^| )(а)ллея( |$)/i, '$1$2лея$3')
+                        .replace(/(^| )(н)абережная( |$)/i, '$1$2абережна$3')
+                        .replace(/(^| )(м)икрорайон( |$)/i, '$1$2ікрорайон$3')
+                        .replace(/(^| )(л)иния( |$)/i, '$1$2інія$3')
+                        .replace(/(^| )(а)кадемика( |$)/i, '$1$2кадеміка$3')
+                        .replace(/(^| )(а)дмирала( |$)/i, '$1$2дмірала$3')
+                        .replace(/ и /i, ' та ');
+                }),
+                new Rule('Mistake in short status', function (t) {
+                    return t
+                        .replace(/(^| )(буль?в?\.?|б-р\.)( |$)/i, '$1б-р$3')
+                        .replace(/(^| )пр-к?т\.?( |$)/i, '$1просп.$2')
+                        .replace(/(^| )пр-з?д\.?( |$)/i, '$1пр.$2')
+                        .replace(/(^| )ул\.?( |$)/i, '$1вул.$2')
+                        .replace(/(^| )р-н\.( |$)/i, '$1р-н$2')
+                        .replace(/(^| )пер\.?( |$)/i, '$1пров.$2')
+                        .replace(/(^| )(пров|туп|просп|пр|вул|ш|пл|ст|ал|мкрн|наб|дор)( |$)/i, '$1$2.$3')
+                }),
+                new Rule('Long status must be short', function (t) {
+                    // Do short status only if there no other shorten statuses in name
+                    return hasShortStatus(t) ? t : t
+                        .replace(/(^| )район( |$)/i, '$1р-н$2')
+                        .replace(/(^| )бульвар( |$)/i, '$1б-р$2')
+                        .replace(/(^| )провулок( |$)/i, '$1пров.$2')
+                        .replace(/(^| )туп[іи]к( |$)/i, '$1туп.$2')
+                        .replace(/(^| )проспект( |$)/i, '$1просп.$2')
+                        .replace(/(^| )проїзд( |$)/i, '$1пр.$2')
+                        .replace(/(^| )вулиця( |$)/i, '$1вул.$2')
+                        .replace(/(^| )шосе( |$)/i, '$1ш.$2')
+                        .replace(/(^| )площа( |$)/i, '$1пл.$2')
+                        .replace(/(^| )станція( |$)/i, '$1ст.$2')
+                        .replace(/(^| )алея( |$)/i, '$1ал.$2')
+                        .replace(/(^| )мікрорайон( |$)/i, '$1мкрн.$2')
+                        .replace(/(^| )набережна( |$)/i, '$1наб.$2')
+                        .replace(/(^| )дорог[аи]( |$)/i, '$1дор.$2')
+                }),
+                new Rule('Shorten street name or status must be long', function (t) {
+                    return t
+                        .replace(/(^| )тр-т\.?( |$)/i, '$1тракт$2')
+                        .replace(/(^| )(сп\.?|узв\.?|узвоз)( |$)/i, '$1узвіз$3')
+                        .replace(/(^| )ген\.?( |$)/i, '$1Генерала$2')
+                        .replace(/(^| )див\.?( |$)/i, '$1Дивізії$2')
+                        .replace(/(^| )ак\.?( |$)/i, '$1Академіка$2')
+                        .replace(/(^| )марш\.?( |$)/i, '$1Маршала$2')
+                        .replace(/(^| )адм\.?( |$)/i, '$1Адмірала$2')
+                }),
+                new Rule('Incorrect number ending', function (t) {
+                    return t
+                        .replace(/-[гштм]а/, '-а')
+                        .replace(/-[ыоиі]й/, '-й')
+                        .replace(/-тя/, '-я')
+                        .replace(/-ая/, '-а')
+                }),
+                new Rule('Incorrect highway name', function (text) {
+                    return text.replace(/([РрНнМмPpHM])[-\s]*([0-9]{2})/, function (a, p1, p2) {
+                        p1 = p1
+                            .replace('р', 'Р')
+                            .replace('н', 'Н')
+                            .replace('м', 'М')
+                            .replace('P', 'Р')
+                            .replace('p', 'Р')
+                            .replace('H', 'Н')
+                            .replace('M', 'М');
+
+                        return p1 + '-' + p2;
+                    });
+                }),
+                new Rule('Incorrect local street name', function (text) {
+                    return text.replace(/([ТтT])[-\s]*([0-9]{2})[-\s]*([0-9]{2})/, function (a, p1, p2, p3) {
+                        p1 = p1
+                            .replace('т', 'Т')
+                            .replace('T', 'Т');
+
+                        return p1 + '-' + p2 + '-' + p3;
+                    });
+                }),
+                new Rule('Incorrect international highway name', function (text) {
+                    return text.replace(/^ *[eе][- ]*([0-9]+)/i, 'E$1');
+                }),
+                new Rule('Incorrect local road name', function (text) {
+                    return text.replace(/([OoCcОоСс])[-\s]*([0-9]+)[-\s]*([0-9]+)[-\s]*([0-9]+)/, function (a, p1, p2, p3, p4) {
+                        p1 = p1
+                            .replace('o', 'О')
+                            .replace('O', 'О')
+                            .replace('c', 'С')
+                            .replace('C', 'С');
+
+                        return p1 + p2 + p3 + p4;
+                    });
+                }),
+
+                new Rule('Fix status', function (t) {
+                    return hasStatus(t) ? t : 'вул. ' + t;
+                }, 'Ukraine'),
+
+                new Rule('Move status to begin of name', function (text) {
+                    var excludeList = /(?: |^)до |(?: |^)на /;
+                    if (! new RegExp(excludeList).test(text)) {
+                        return text.replace(/(.*)(вул\.)(.*)/, '$2 $1 $3');
+                    }
+                    return text;
+                }, 'Ukraine'),
             ]);
         };
 
