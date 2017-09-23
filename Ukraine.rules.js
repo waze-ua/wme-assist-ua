32ac927253b25ce5cfbce4906f4df60c314f4452
Alexander Shashkevich <alex@stunpix.com>
2017-09-24 00:05:48 +0300

    Rename variable

--- Ukraine.rules.js
+++ Ukraine.rules.js
@@ -1,8 +1,8 @@
-var WME_Assist = WME_Assist || {};
-WME_Assist.countryCode = 'UA';
-WME_Assist.rulesVersion = '2017.09.23';
-WME_Assist.rulesVariants = ['Ukraine', 'Lviv'];
-WME_Assist.rules = function () {
+var WME_Assist_Country = WME_Assist_Country || {};
+WME_Assist_Country.code = 'UA';
+WME_Assist_Country.rulesVersion = '2017.09.23';
+WME_Assist_Country.rulesVariants = ['Ukraine', 'Lviv'];
+WME_Assist_Country.rules = function () {
     var hasCyrillic = function(s) {return s.search(/[а-яіїєґ]/i) != -1;};
     var hasShortStatus = function(s) { return s.search(/( |^)(вул\.|просп\.|мкрн\.|наб\.|пров\.|ст\.|пр\.|дор\.|б-р|р-н)( |$)/i) != -1; };
     var hasLongStatus = function(s) { return s.search(/( |^)(площа|алея|шосе|тракт|узвіз|тупик|міст|в\'їзд|виїзд|виїзд|розворот|трамвай|залізниця|майдан|заїзд|траса|шляхопровід|шлях|завулок|квартал)( |$)/i) != -1; };
